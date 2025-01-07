605c785404a96e0f2a09d6b6a2517435708eef20
Nahid-Hasan-Sourav <nahidhasansourav05@gmail.com>
2025-01-07 17:58:15 +0600

    added a textInput components

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import Button from "../components/Button";
+import TextInput from "../components/TextInput";
 
 
 export default function Login() {
@@ -11,6 +12,14 @@ export default function Login() {
         text="Login"
         className="text-2xl bg-blue-500 hover:bg-blue-700"
       />
+      <TextInput
+  value="This is disabled"
+  onChange={() => {}}
+  placeholder="Cannot change this"
+  disabled={true}
+  className="bg-gray-200 border-gray-300"
+/>
+
     </div>
   );
 }
